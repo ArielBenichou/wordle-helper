f26bf10d467f94479e99d1c28c7f67ca02b54b4c
Ariel Benichou <arielb@balink.net>
2022-02-02 20:35:34 +0200

    added github link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./index.module.css";
+import GitHubIcon from "../../assets/github.svg?component";
 
 function Footer() {
-  return <p className={styles.copyright}>© Ariel Benichou</p>;
+  const navigateToGithub = () => {
+    window.open("https://github.com/ArielBenichou/wordle-helper", "_blank");
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.copyright}>© Ariel Benichou</p>
+      <GitHubIcon className={styles.github} onClick={navigateToGithub} />
+    </div>
+  );
 }
 
 export default Footer;
